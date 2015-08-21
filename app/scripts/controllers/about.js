844ff79c670bda419362d82ea3d06099c4293220
Oscar Cortez <om.cortez.2010@gmail.com>
2015-08-20 23:46:35 -0600

    the first Hyrax bytes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hyraxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hyraxApp
+ */
+angular.module('hyraxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hyraxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hyraxApp
+ */
+angular.module('hyraxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
