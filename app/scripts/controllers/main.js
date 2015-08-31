3613ec7fddb17f2750b31d9e99ec6821ec812f3b
Oscar Cortez <om.cortez.2010@gmail.com>
2015-08-31 15:47:01 -0600

    some functions for sponsor type and mount

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,17 +15,35 @@ angular.module('hyraxApp')
     function($scope, $mdSidenav, $http){
 
       $http.get('/data/elements.json')
-      .success(function(data, status, headers, config) {
-        $scope.posts = data;
-        console.log(data);
+      .success(function(data, status) {
+        $scope.elements = data;
+        console.log(data, status);
       })
-      .error(function(data, status, headers, config) {
-        // log error
-        console.log('Bug');
+      .error(function(data, status) {
+        console.log(data, status);
       });
 
       $scope.toggleSidenav = function(menuId) {
         $mdSidenav(menuId).toggle();
       };
+
+      $scope.addElement = function(element) {
+        console.log(element);
+  	    $scope.mount = totalMount(element);
+        $scope.type = sponsorType();
+      };
+
+      var totalMount = function(element) {
+        var total = [];
+        total += element;
+        return total;
+      };
+
+      var sponsorType = function() {
+        var type = 'Juanito';
+        return type;
+      };
+
     }
+
   ]);
